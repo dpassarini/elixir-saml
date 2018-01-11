dd3878b0142534bd648891c81744e2db5bbf0851
Daniel de Carvalho Passarini <daniel.passarini@gmail.com>
2018-01-11 20:58:41 -0200

    first commit

---
+++ lib/elixir_saml.ex
@@ -0,0 +1,18 @@
+defmodule ElixirSaml do
+  @moduledoc """
+  Documentation for ElixirSaml.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirSaml.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_saml_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirSamlTest do
+  use ExUnit.Case
+  doctest ElixirSaml
+
+  test "greets the world" do
+    assert ElixirSaml.hello() == :world
+  end
+end
