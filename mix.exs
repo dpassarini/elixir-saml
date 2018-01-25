25f98ef35bd0765f4603c00737fcc3a4bf9b258c
Daniel de Carvalho Passarini <daniel.passarini@gmail.com>
2018-01-25 19:58:45 -0200

    [ WIP ] removing unused dependency

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule ElixirSaml.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :httpoison, :uuid, :floki]
+      extra_applications: [:logger, :httpoison, :uuid]
     ]
   end
 
